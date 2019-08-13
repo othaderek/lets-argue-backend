433424eb4065f9923a472de73ec884c46d0f2c4c
othaderek <otha.hernandez@gmail.com>
2019-08-13 14:16:53 -0400

    Comment creation added.

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::CommentsController < ApplicationController
+
+  def create
+    comment = Comment.create(comment_params)
+    if comment.valid?
+    else
+      render json: {errors: comment.errors.full_messages}, status: unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def comment_params
+    params.permit(:commentable_type, :commentable_id, :user_id, :body)
+  end
+
 end
