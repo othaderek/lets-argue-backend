029bb4c887f177b57908bc10e40dfe2d001388d2
othaderek <otha.hernandez@gmail.com>
2019-08-22 10:21:18 -0400

    create post finished

--- app/controllers/api/v1/channels_controller.rb
+++ app/controllers/api/v1/channels_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::ChannelsController < ApplicationController
+
+  def index
+    @channels = Channel.all
+    render json: @channels
+  end
+
+  def show
+    @channel = Channel.find(params[:id])
+    render json: @channel, include: "**"
+  end
+
 end
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -11,4 +11,21 @@ class Api::V1::PostsController < ApplicationController
     render json: @posts, include: "**"
   end
 
+  def create
+    @post = Post.create(post_params)
+    if @post.valid?
+
+      render json: @post
+    else
+      render json: {errors: @post.errors.full_messages}, status: unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def post_params
+    params.permit(:user_id, :channel_id, :body, :title)
+  end
+
 end
